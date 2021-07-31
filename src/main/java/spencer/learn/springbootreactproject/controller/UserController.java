4e41fc14f57ae0da36f29f99e2c83e34e8c815a8
Spencer <spencer676@hotmail.com>
2021-07-31 12:06:22 +0200

    fixed cross origin error telling controller where to expect calls from.

--- src/main/java/spencer/learn/springbootreactproject/controller/UserController.java
+++ src/main/java/spencer/learn/springbootreactproject/controller/UserController.java
@@ -1,6 +1,7 @@
 package spencer.learn.springbootreactproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import spencer.learn.springbootreactproject.repository.UserRepository;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/")
 public class UserController {
