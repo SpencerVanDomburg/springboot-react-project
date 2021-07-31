62920e9ef9d00ab8aca1581cef191b7eaaabf4a6
Spencer <spencer676@hotmail.com>
2021-07-31 10:40:28 +0200

    created project with spring initializr

---
+++ src/main/java/spencer/learn/springbootreactproject/SpringbootReactProjectApplication.java
@@ -0,0 +1,13 @@
+package spencer.learn.springbootreactproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spencer/learn/springbootreactproject/SpringbootReactProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package spencer.learn.springbootreactproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
